fa9b28f3db1cf286de3541960c19cbb1132dde05
Brooke <you@example.com>
2023-10-23 11:43:56 -0600

    changed code to find number and letter counts

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-
+def count_letters_and_numbers(filename):
+ letter_count = 0
+ number_count = 0
 # context manager
-with open("mydefaults.ini.txt") as ini_file:
-    data = ini_file.read()
+ with open("mydefaults.ini.txt") as ini_file:
+   for line in ini_file:
+      for char in line:
+         if char.isalpha():
+            letter_count += 1
+         elif char.isdigit(): 
+            number_count += 1
 
-lines = data.split("\n")
-key_value_ini = dict()
-# print(len(lines))
-for line in lines:
-    if "=" in line:
-        fields = line.split("=")
-        key_value_ini[fields[0]] = fields[1]
-print(key_value_ini)
+ return letter_count, number_count
+
+ini_file = 'C:\\Users\\yukur\\Downloads\\mydefaults.ini.txt'
+total_letters, total_numbers = count_letters_and_numbers(ini_file)
+
+print(f"Total letters is: {total_letters}")
+print(f"Total numbers is: {total_numbers}")
